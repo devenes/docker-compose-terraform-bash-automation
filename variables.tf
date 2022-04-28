983cd49ad270dbdde9618327dfdbbc2d8f28c492
Enes <66560757+devenes@users.noreply.github.com>
2022-04-29 02:07:15 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,31 @@
+####################################
+#### Required AWS Configuration ####
+####################################
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "profile" {
+  default = "default"
+}
+
+########################################
+# Required AWS Resources Configuration #
+########################################
+
+variable "key_name" {
+  default = "east1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "instance_name" {
+  default = "Docker Web Server"
+}
+
+variable "security_group_name" {
+  default = "docker-sec-gr"
+}
